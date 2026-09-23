a2f026650ba4bc1e4f1c8db2cdf575e8559f4c8d
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-09-23 18:08:22 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-const gradiCACHE = 'v6';
+const gradiCACHE = 'v7';
 const ASSETS = [
-  './',
-  './index.html',
-  './script.js',
-  './style.css',
-  './manifest.json',
-  './icon512.png',
-  './icon192.png'
+  'index.html',
+  'script.js',
+  'style.css',
+  'manifest.json',
+  'icon512.png',
+  'icon192.png'
 ];
 
 self.addEventListener(
